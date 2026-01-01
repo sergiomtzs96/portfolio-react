28b9cc1856afad536e64410c2d36dea51b762296
Sergio Alberto Martínez Salmerón <sergiomtzs1996@gmail.com>
2026-01-02 00:03:55 +0100

    cambios frontend

--- src/components/sections/AboutMe.jsx
+++ src/components/sections/AboutMe.jsx
@@ -8,45 +8,105 @@ import coding from '../../img/coding.svg';
 import ray from '../../img/ray.svg';
 import palette from '../../img/palette.svg';
 
-export default function AboutMe() {
-    return (
-        <div className='bg-gray-100'>
-            <div id='aboutme' className="grid grid-cols-2 grid-rows-1 gap-4 max-w-7xl mx-auto" data-aos="fade-up">
-                <div className='flex flex-col h-full justify-center'>
+import { CodeXml, ServerCog, Palette, Users, Medal, Coffee } from 'lucide-react'
 
-                    <div className='max-w-3xl flex flex-col w-full items-center p-10 text-center'>
-                        <div className="w-full">
-                            <h1 className="text-3xl font-bold mb-5">Acerca de mí</h1>
-                            <p className="text-2xl text-gray-500">Soy un desarrollador apasionado con 1 año de experiencia creando proyectos propios y con ganas de emprender mi carrera profesional</p>
-                        </div>
-                        <div className='mt-10 w-full flex flex-col items-center text-center brightness-50'>
-                            <img className='w-full max-w-2xl rounded-xl' src={code} alt="code" />
-                        </div>
+import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card"
+import DomeGallery from '../gsap/DomeGallery'
+
+export default function AboutMe() {
+
+    const skills = [
+        {
+            icon: <CodeXml size={40} color='purple' />,
+            title: 'Frontend',
+            description: 'Experto en React y tecnologías modernas de frontend'
+        },
+        {
+            icon: <ServerCog size={40} color='yellow' />,
+            title: 'Backend',
+            description: 'Especializado en Node.js, Python y bases de datos modernas'
+        },
+        {
+            icon: <Palette size={40} color='blue' />,
+            title: 'Diseño UI/UX',
+            description: 'Creación de interfaces intuitivas y experiencias de usuario excepcionales.'
+        }
+    ]
+
+    const data = [
+        {
+            icon: <Users />,
+            title: 'Proyectos Completados',
+            data: 3
+        },
+        {
+            icon: <Medal />,
+            title: 'Años de experiencia',
+            data: 1.5
+        },
+        {
+            icon: <Coffee />,
+            title: 'Tazas de Café',
+            data: 16.522
+        }
+    ]
+
+
+    return (
+        <div className='bg-[#11057E]'>
+            <div id='aboutme' className="grid grid-cols-2 grid-rows-1 gap-4 w-full mx-auto px-6" data-aos="fade-up">
+                <div className='flex flex-col h-full justify-center'>
+                    <div style={{ width: '50vw', height: '100vh' }}>
+                        <DomeGallery />
                     </div>
                 </div>
                 <div className='flex flex-col items-center text-justify p-10 w-full'>
-                    <div className='' data-aos="fade-down">
-                        <div className='w-full '>
-                            <h5 className='text-2xl font-bold text-center'>Mi historia</h5>
-                            <p className='mt-5'> Comencé mi viaje en el desarrollo web hace más de 5 años. Mi pasión por resolver problemas complejos y crear experiencias digitales excepcionales me ha llevado a especializarme en tecnologías modernas como React, Node.js y cloud computing.
+                    <Card className={'bg-[#11057E] text-white'}>
+                        <CardHeader>
+                            <CardTitle className={'flex flex-col gap-4 space-y-4'}>
+                                <h2 className='text-2xl font-bold text-center'>Mi Historia</h2>
+                                <p>Comencé mi viaje en el desarrollo web hace más de 5 años. Mi pasión por resolver problemas complejos y crear experiencias digitales excepcionales me ha llevado a especializarme en tecnologías modernas como React, Node.js y cloud computing.
 
-                                Me encanta trabajar en proyectos desafiantes donde puedo combinar creatividad con tecnología para crear soluciones que realmente impacten a los usuarios. </p>
-                        </div>
-                    </div>
-                    <div className='w-full flex items-center justify-center gap-3' data-aos="fade-right">
-                        <div className='flex flex-col lg:flex-row gap-5 justify-between w-full lg:max-w-2xl'>
-                            <Stats icon={users} value="3" description="Proyectos completados" />
-                            <Stats icon={medal} value="2" description="Años de experiencia" />
-                            <Stats icon={coffee} value="20.000" description="Tazas de café" />
-                        </div>
-                    </div>
-                    <div className='w-full flex flex-col items-center p-3' data-aos="fade-left">
-                        <div className='w-full justify-center lg:max-w-2xl'>
-                            <Campos icon={coding} campo="Desarrollo Frontend" description="Experto en React, Vue.js y tecnologías modernas de frontend" />
-                            <Campos icon={ray} campo="Desarrollo Backend" description="Especializado en Node.js, Python y bases de datos modernas" />
-                            <Campos icon={palette} campo="Diseño UI/UX" description="Creación de interfaces intuititvas y experiencias de usuario excepcionales" />
-                        </div>
-                    </div>
+                                    Me encanta trabajar en proyectos desafiantes donde puedo combinar creatividad con tecnología para crear soluciones que realmente impacten a los usuarios.</p>
+                            </CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <div className='w-full flex items-center justify-center gap-3 pb-6 h-50' data-aos="fade-right">
+                                {data.map((data, j) => (
+                                    <Card className={'w-full h-full justify-between'}>
+                                        <CardHeader>
+                                            <CardTitle>
+                                                <div className='flex flex-col gap-4 items-center justify-center text-justify'>
+                                                    <div>{data.icon}</div>
+                                                    <p className='text-center'>{data.title}</p>
+                                                </div>
+                                            </CardTitle>
+                                        </CardHeader>
+                                        <CardContent className={'flex justify-center'}>
+                                            <p className='text-2xl font-bold'>{data.data}</p>
+                                        </CardContent>
+                                    </Card>
+                                ))}
+                            </div>
+                            <div className='flex flex-col items-center justify-center gap-4' data-aos="fade-left">
+                                {skills.map((skill, i) => (
+                                    <Card className={'w-full'}>
+                                        <CardHeader>
+                                            <CardTitle className={'flex justify-center'}>
+                                                <div className='flex gap-4 items-center'>
+                                                    {skill.icon}
+                                                    <p>{skill.title}</p>
+                                                </div>
+                                            </CardTitle>
+                                        </CardHeader>
+                                        <CardContent>
+                                            <p className='text-center'>{skill.description}</p>
+                                        </CardContent>
+                                    </Card>
+                                ))}
+                            </div>
+                        </CardContent>
+                    </Card>
                 </div>
             </div>
         </div>
